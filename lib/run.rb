5ddabd5c0f1757f0a8338bfd4f4c708c8dfdc056
ngouti <gouti.naama@gmail.com>
2018-10-16 21:27:01 -0500

    ng

---
+++ lib/run.rb
@@ -0,0 +1,13 @@
+require 'pry'
+require './project.rb'
+require './backer.rb'
+
+emma = Backer.new("Emma")
+
+better_uber = Project.new("A Better Uber")
+
+eddie = Backer.new("Eddie")
+
+baseball = Project.new("Baseball For All")
+
+/binding.pry
